60bed6bb50346cc9c2264d3caca166c01209db9d
sorokinilya <ilya.sorokin@sigmaukraine.com>
2018-06-12 11:40:09 +0300

    no message

---
+++ NavTest/NavTest/ViewModels/Items/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NavTest.ViewModels.Items
+{
+    public struct Item
+    {
+        public readonly string id;
+        public readonly string text;
+        public readonly string description;
+
+    }
+}
